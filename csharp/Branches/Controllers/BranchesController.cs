39f0680ad2503a7122dd0578a855d6aa18c527aa
surikadt <gnidanol@gmail.com>
2024-09-27 23:14:38 +0300

    Implemented api for branches with centrilized exception handlind

---
+++ csharp/Branches/Controllers/BranchesController.cs
@@ -0,0 +1,74 @@
+using Branches_BLL.DTOs.Branch;
+using Branches_BLL.DTOs.BranchMember;
+using Branches_BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Branches.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BranchesController : ControllerBase
+{
+    private readonly IBranchService _branchService;
+
+    public BranchesController(IBranchService branchService)
+    {
+        _branchService = branchService;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<BranchDTO>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)] 
+    public async Task<ActionResult<IEnumerable<BranchDTO>>> GetAllBranches()
+    {
+        var branches = await _branchService.GetAllBranchesAsync();
+        return Ok(branches);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(BranchDTO), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)] 
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)] 
+    public async Task<ActionResult<BranchDTO>> GetBranchById(int id)
+    {
+        var branch = await _branchService.GetBranchByIdAsync(id);
+        return Ok(branch);
+    }
+
+    [HttpGet("{branchId}/moderators")]
+    public async Task<ActionResult<IEnumerable<BranchMemberDTO>>> GetModerators(int branchId)
+    {
+        var moderators = await _branchService.GetModeratorsByBranchIdAsync(branchId);
+        return Ok(moderators);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)] 
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)] 
+    public async Task<IActionResult> AddBranch([FromBody] CreateBranchDTO branchDto)
+    {
+        await _branchService.AddBranchAsync(branchDto);
+        return CreatedAtAction(nameof(GetBranchById), new { id = branchDto.Name }, branchDto);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)] 
+    [ProducesResponseType(StatusCodes.Status404NotFound)] 
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)] 
+    public async Task<IActionResult> UpdateBranch(int id, [FromBody] CreateBranchDTO branchDto)
+    {
+        await _branchService.UpdateBranchAsync(id, branchDto);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)] 
+    [ProducesResponseType(StatusCodes.Status404NotFound)] 
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)] 
+    public async Task<IActionResult> DeleteBranch(int id)
+    {
+        await _branchService.DeleteBranchAsync(id);
+        return NoContent();
+    }
+}
---
+++ csharp/Branches/Program.cs
@@ -0,0 +1,61 @@
+using System.Data;
+using Branches_BLL.AutomapperProfiles;
+using Branches_BLL.Services;
+using Branches_BLL.Services.Interfaces;
+using Branches_DAL.Repositories;
+using Branches_DAL.Repositories.Interfaces;
+using Branches_DAL.UoW;
+using Branches_DAL.UoW.Interfaces;
+using Branches.Middleware.Exceptions;
+using Npgsql;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Database + transactions
+var connectionString = builder.Configuration.GetConnectionString("PgsqlConnection");
+builder.Services.AddScoped<NpgsqlConnection>(_ => new NpgsqlConnection(connectionString));
+builder.Services.AddScoped<IDbTransaction>(s =>
+{
+    var connection = s.GetRequiredService<NpgsqlConnection>();
+    connection.Open();
+    return connection.BeginTransaction();
+});
+
+// Repositories + UoW
+builder.Services.AddScoped<IBranchRepository, BranchRepository>();
+builder.Services.AddScoped<IBranchMemberRepository, BranchMemberRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Services
+builder.Services.AddScoped<IBranchService, BranchService>();
+
+// Automapper
+builder.Services.AddAutoMapper(typeof(BranchMappingProfile).Assembly);
+
+// Exception Handling
+builder.Services.AddExceptionHandler<NotFoundExceptionHandler>();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseExceptionHandler();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
