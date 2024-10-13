35a3e36d572522376beefac0bb44880a1c807bc7
surikadt <gnidanol@gmail.com>
2024-10-13 18:12:14 +0300

    Conteinerized apis and gateway

--- gateway/src/main/java/com/branches/gateway/Routes.java
+++ gateway/src/main/java/com/branches/gateway/Routes.java
@@ -11,18 +11,24 @@ import org.springframework.web.servlet.function.ServerResponse;
 
 @Configuration(proxyBeanMethods = false)
 public class Routes {
-	
+
 	@Bean
 	public RouterFunction<ServerResponse> commentsRoute() {
 		return route("comments")
-			.route(RequestPredicates.path("/api/comments"), http("http://localhost:8081"))
+			.route(RequestPredicates.path("/api/comments")
+				.or(RequestPredicates.path("/api/comments/{id}"))
+				.or(RequestPredicates.path("/api/comments/posts/{id}")),
+				http(System.getenv("COMMENTS_URL")))
 			.build();
 	}
+
 
 	@Bean
 	public RouterFunction<ServerResponse> reportsRoute() {
 		return route("reports")
-			.route(RequestPredicates.path("/api/reports"), http("http://localhost:8082"))
+			.route(RequestPredicates.path("/api/reports")
+				.or(RequestPredicates.path("/api/reports/{id}")),
+				http(System.getenv("REPORTS_URL")))
 			.build();
 	}
 }
